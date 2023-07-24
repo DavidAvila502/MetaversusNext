5ebf383c2dbba40ba9275052addffd515627b145
David Avila <davidavila6002@gmail.com>
2023-07-24 12:32:15 -0600

    get started section done

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -11,6 +11,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => {
          className={`relative ${
             active === id ? "lg:flex-[3.5] flex-[10]" : "lg:flex-[0.5] flex-[2]"
          } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
+         onClick={() => handleClick(id)}
       >
          <img
             src={imgUrl}
@@ -37,6 +38,9 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => {
                <p className="font-normal text-[16px] leading-[20px] text-white uppercase">
                   Enter the Metaverse
                </p>
+               <h2 className="mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white">
+                  {title}
+               </h2>
             </div>
          )}
       </motion.div>
