6f8cbe7efc30a3a6e820ef7b7308ebaac186a809
David Avila <davidavila6002@gmail.com>
2023-07-25 12:44:15 -0600

    What's new section done

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState } from "react";
 import { motion } from "framer-motion";
 import styles from "../styles";
 import { staggerContainer, fadeIn, planetVariants } from "../utils/motion";
